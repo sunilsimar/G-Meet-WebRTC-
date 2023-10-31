9ea054e3bac0cecc82d5d99a023749837e6b8609
Sunil <sunisimar2002d@gmail.com>
2023-10-31 12:22:07 +0530

    added RTCPeerConnection

--- Client-Side/components/MeetingPage.jsx
+++ Client-Side/components/MeetingPage.jsx
@@ -1,11 +1,14 @@
-import socketIO from "socket.io-client"
+import socketIO, { Socket } from "socket.io-client"
 import { useState } from "react";
-import { Video } from "./Video"
+import { useParams } from "react-router-dom";
+import { Video } from "./Video";
 import { useEffect } from "react";
+import { CentralizedCard } from "./CentralizedCard";
+import { Button, Grid, Typography } from "@mui/material";
 
 export function MeetingPage() {
     const [socket, setSocket] = useState(null);
-    const [meeting, setMeeting] = useState(false);
+    const [meetingJoined, setMeetingJoined] = useState(false);
     const [videoStream, setVideoStream] = useState();
     const [remoteVideoStream, setRemoteVideoStream] = useState();
 
@@ -28,13 +31,111 @@ export function MeetingPage() {
 
             s.on("localDescription", async ({ description }) => {
                 //Receiving video
-                let pc = new RTCPeerConnection({
-                    iceServers: {
+                // let pc = new RTCPeerConnection({
+                //     iceServers: [
+                //         {
+                //             urls: "stun:stun.l.google.com:19302",
+                //         },
+                //     ],
+                // });
 
-                    }
-                })
-            })
-        })
-    })
+                pc.setRemoteDescription(description);
+                pc.ontrack = (e) => {
+                    setRemoteVideoStream(new MediaStream([e.track]))
+                }
+
+                s.on("iceCandidate", ({ candidate }) => {
+                    pc.addIceCandidate(candidate)
+                });
+
+                pc.onicecandidate = ({ candidate }) => {
+                    s.emit("iceCandidateReply", { candidate });
+                }
+
+                await pc.setLocalDescription(await pc.createAnswer());
+                s.emit("remoteDescription", { description: pc.localDescription });
+            });
+
+            s.on("remoteDescription", async ({ description }) => {
+                // Receiving video -
+                console.log({ description });
+                pc.setRemoteDescription(description);
+                pc.ontrack = (e) => {
+                    setRemoteVideoStream(new MediaStream([e.track]));
+                };
+                s.on("iceCandidate", ({ candidate }) => {
+                    pc.addIceCandidate(candidate);
+                });
+
+                pc.onicecandidate = ({ candidate }) => {
+                    s.emit("iceCandidateReply", { candidate });
+                };
+
+            });
+        });
+    }, []);
+
+    if (!videoStream) {
+        return <div>
+            Loading......
+        </div>
+    }
+
+    if (!meetingJoined) {
+        return <div style={{ minHeight: "100vh", }}>
+            <CentralizedCard>
+                <div>
+                    <Typography textAlign={"center"} variant="h5">
+                        Hi welcome to meeting {roomId}.
+                    </Typography>
+                </div>
+                <br></br>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <Button onClick={async () => {
+                        //sending pc
+                        let pc = new RTCPeerConnection({
+                            iceServers: [
+                                {
+                                    urls: "stun:stun.l.google.com:19302"
+                                },
+                            ],
+                        });
+                        pc.onicecandidate = ({ candidate }) => {
+                            socket.emit("iceCandidate", { candidate });
+                        }
+                        pc.addTrack(videoStream.getVideoTracks()[0])
+                        pc.onnegotiationneeded = async () => {
+                            try {
+                                await pc.setLocalDescription(await pc.createOffer());
+                                console.log(pc.localDescription);
+                                socket.emit("localDescription", { description: pc.localDescription });
+                            } catch (err) {
+                                console.log(err);
+                            }
+                        };
+
+                        // socket.on("remoteDescription", async({description})=>{
+                        //     await pc.setRemoteDescription(description)
+                        // });
+                        // socket.on("iceCandidateReply",({candidate})=>{
+                        //     pc.addIceCandidate(candidate)
+                        // });
+                        setMeetingJoined(true);
+                    }} disabled={!socket} variant="contained">
+                        Join Meeting
+                    </Button>
+                </div>
+            </CentralizedCard>
+        </div>
+    }
+    console.log({ remoteVideoStream, videoStream })
+    return <Grid container spacing={2} alignContent={"center"} justifyContent={"center"}>
+        <Grid item xs={12} md={6} lg={4}>
+            <Video stream={videoStream} />
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+            <Video stream={remoteVideoStream} />
+        </Grid>
+    </Grid>
 
 }
