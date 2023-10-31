8c0aa5dc6b987e179aa5131944ab10f65270c847
Sunil <sunisimar2002d@gmail.com>
2023-10-31 12:26:03 +0530

    updated Routes

--- Client-Side/src/App.jsx
+++ Client-Side/src/App.jsx
@@ -3,16 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LandingPage } from "../components/LandingPage";
 import { CentralizedCard } from "../components/CentralizedCard";
 import { Appbar } from "../components/Appbar";
+import { MeetingPage } from "../components/MeetingPage";
 
 function App() {
   return (
     <Router>
       <Appbar />
       <Routes>
-        {/* <Route path='/meetiing/:roomId' element={<MeetingPage />} /> */}
+        <Route path='/meeting/:roomId' element={<MeetingPage />} />
         <Route path='*' element={<LandingPage />} />
-        <Route path='/Card' element={<CentralizedCard />} />
-
       </Routes>
     </Router>
   )
